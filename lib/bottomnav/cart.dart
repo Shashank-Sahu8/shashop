7ada60e2974e6bc094bf6ea22deb3967795e9cad
Shashank-Sahu8 <shashanksahu87070@gmail.com>
2023-11-01 18:30:07 +0530

    t commit

--- lib/bottomnav/cart.dart
+++ lib/bottomnav/cart.dart
@@ -1,4 +1,7 @@
+import 'package:finallyshop/pages/login_page.dart';
 import 'package:finallyshop/pages/search.dart';
+import 'package:finallyshop/pages/signup_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:finallyshop/main.dart';
 
@@ -10,6 +13,8 @@ class cart extends StatefulWidget {
 }
 
 class _cartState extends State<cart> {
+  final auth=FirebaseAuth.instance;
+
   void idd(indexx){
 
   }
@@ -22,6 +27,9 @@ class _cartState extends State<cart> {
         shadowColor: Colors.white38,
         title: Text("My Cart",style: TextStyle(color: Colors.black87,fontSize: 24),),
         actions: [
+          IconButton(onPressed: (){
+            auth.signOut().then((value){Navigator.push(context, MaterialPageRoute(builder: (context)=>login()));}).onError((error, stackTrace){utils().toastmess(error.toString());});
+          }, icon: Icon(Icons.logout,color: Colors.blueGrey,)),
            IconButton(highlightColor: Color(0xfffa8128),onPressed: (){showSearch(context: context, delegate: Search());}, icon: Icon(Icons.search,color: Colors.blueGrey,size: 30,)),
           
   ],
